b9799e774bc75af6de4b67551eb4f85f0bf02ed5
Iago Augusto <iago.rivelo@gmail.com>
2023-11-26 00:19:16 -0300

    :sparkles: feat(page.tsx) Instalando a biblioteca Lucide React + Finalizando Projeto com consumo da API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,34 @@ import { Search } from 'lucide-react';
 export default function Home() {
 
   const [input, setInput] = useState('');
+  const [cep, setCep]     = useState({});
 
-  // const data = await fetch('https://viacep.com.br/ws/50070075/json');
-  
-  // const result = await data.json();
-  
-  // console.log(result);
+  async function search() {
+    if(input === '') {
+      alert("Preencha algum CEP!");
+
+      return;
+    }
+
+    const data = await fetch(`https://viacep.com.br/ws/${input}/json`);
+
+    const response = await data.json();
+
+    setCep(response);
+
+    setInput('');
+
+      // verification(response);
+  }
+
+  // function verification(response) {
+  //   if (Object.keys(response.data).length === 1) {
+  //     alert("Verifique o campo preenchido e tente novamente!");
+  //   }
+  // }
 
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center bg-violet-800">
+    <main className="min-h-screen flex flex-col items-center justify-center bg-[#121214]">
       <h1 className="text-white text-4xl font-semibold">Ache o Endereço através do CEP</h1>
 
       <div className='bg-zinc-700 p-4 my-9 mx-0 rounded flex'>
@@ -25,10 +44,20 @@ export default function Home() {
         value={input} 
         onChange={ (e) => setInput(e.target.value) } />
 
-        <button className='bg-transparent border-none flex justify-center items-center cursor-pointer transition-transform'>
-          <Search className='text-white' />
+        <button className='bg-transparent border-none flex justify-center items-center cursor-pointer transition-transform' onClick={search}>
+          <Search className='text-white hover:scale-110' />
         </button>
       </div>
+
+      {Object.keys(cep).length > 1 && (
+        <div className='flex justify-center items-start flex-col text-white bg-violet-700 w-96 px-7 rounded'>
+          <h2 className='my-4 font-bold text-3xl'>CEP: { cep.cep }</h2>
+          <span className='mb-4 font-bold'>{ cep.logradouro }</span>
+          <span className='mb-4 font-bold'>Complemento: { cep.complemento }</span>
+          <span className='mb-4 font-bold'>Bairro: { cep.bairro }</span>
+          <span className='mb-4 font-bold'>{ cep.localidade } - { cep.uf }</span>
+        </div>
+      )}
     </main>
   )
 }
